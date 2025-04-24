d21ae769eee85e0d47d6dd55ea6bd616dd6fe584
Gaurav96642 <9816957620g@gmail.com>
2025-04-24 13:47:01 +0530

    apilink

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://98.84.233.111:7000/api', // 👈 Replace with your API
+  baseURL: 'http://webserver.greenacres-popcorn.com:7000/api', // 👈 Replace with your API
   headers: {
     'Content-Type': 'application/json',
   },
